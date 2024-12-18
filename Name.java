f0456a4b2bb67f9f11cbba93d3efe588d2d8f349
Professor Lund <75629388+BLCCompSciPKL@users.noreply.github.com>
2024-12-18 10:27:23 -0600

    First commit

---
+++ Name.java
@@ -0,0 +1,16 @@
+/* Hello Assignment Example
+   Must use escape sequences for backslash
+*/
+
+public class Name
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" __  __        _                     _ ");
+        System.out.println("|  \\/  |      | |                   | |");
+        System.out.println("| \\  / |_ __  | |    _   _ _ __   __| |");
+        System.out.println("| |\\/| | '__| | |   | | | | '_ \\ / _` |");
+        System.out.println("| |  | | |    | |____ |_| | | | | (_| |");
+        System.out.println("|_|  |_|_|    |______\\__,_|_| |_|\\__,_|");
+    }
+}
