bfca86fad746c348b075e37a2269d3def43f8f1f
Professor Lund <75629388+BLCCompSciPKL@users.noreply.github.com>
2024-12-18 10:37:14 -0600

    Added Hello Goodbye

--- Name.java
+++ Name.java
@@ -4,13 +4,16 @@
 
 public class Name
 {
+    // Here is the main program in Java!!!!
     public static void main(String[] args)
     {
+        System.out.println("Hello");
         System.out.println(" __  __        _                     _ ");
         System.out.println("|  \\/  |      | |                   | |");
         System.out.println("| \\  / |_ __  | |    _   _ _ __   __| |");
         System.out.println("| |\\/| | '__| | |   | | | | '_ \\ / _` |");
         System.out.println("| |  | | |    | |____ |_| | | | | (_| |");
         System.out.println("|_|  |_|_|    |______\\__,_|_| |_|\\__,_|");
+        System.out.println("Goodbye");
     }
 }
